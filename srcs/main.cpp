abc3b429de047118560d85321bbca28d0baac0c1
ftymchyn <ftymchyn@student.unit.ua>
2019-10-13 15:52:47 +0300

    updated main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,33 +1,17 @@
-#include <iostream>
-
 #include "Common.hpp"
-#include "NcursesOut.hpp"
-#include "IMonitorDisplay.hpp"
+#include "SystemMonitor.hpp"
 
-int main() {
-	IMonitorDisplay *visualMode = new NcursesOut();
-	bool isNcurses = true;
-	Command command;
-	while (true) {
-		visualMode->printOut();
-		command = visualMode->getInput();
-		if (command == SPACE) {
-			// delete(visualMode);
-			if (isNcurses) {
-				// visualMode = new QTOut();
-//				std::cout << "Call graphical" << std::endl;
-				isNcurses = false;
-			}
-			else {
-				visualMode = new NcursesOut();
-				isNcurses = true;
-			}
-		}
-		else if (command == EXIT) {
-			delete(visualMode);
-			break;
-		}
+int main( int ac, char** av )
+{
+	SystemMonitor sysMonitor;
+	eDisplayTarget displayTarget = TERMINAL_WND;
+
+	if ( ac > 1 && static_cast< std::string >( av[ 1 ] ) == "-g" )
+	{
+		displayTarget = GRAPHIC_WND;
 	}
+
+	sysMonitor.run( displayTarget );
 
 	return 0;
 }
